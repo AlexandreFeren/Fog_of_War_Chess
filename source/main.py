291d64dd50124954bd3b405214008c2628d30523
elmbald <alexandreferen@gmail.com>
2021-03-19 14:33:50 -0400

    add turn swap on move

--- source/main.py
+++ source/main.py
@@ -1,8 +1,22 @@
 import ruleset
-
-board = ruleset.Board()
-print(board.get_board_by_file())
-
 import renderer
 
-renderer.draw_board(board.get_board_by_file())
+
+board = ruleset.Board()
+
+
+win = renderer.draw_board(board.get_graphics_board())
+while 1:
+    render_mode = board.to_play
+    #get 2 squares, instantly make move
+    move_made = None
+    last_squares = [None, None]
+    renderer.update_board(win, board.get_graphics_board(), board.get_fog_squares(render_mode))
+
+    while move_made == None:
+        # shift moves by 1, and exits once a valid move is made
+        last_squares[0] = last_squares[1]
+        last_squares[1] = renderer.get_update(win, board.get_graphics_board(), board.get_fog_squares(render_mode))
+        print(last_squares)
+        move_made = board.make_move(last_squares)
+#start game loop, then run an update until a valid move is made
--- source/renderer.py
+++ source/renderer.py
@@ -1,33 +1,36 @@
 from graphics import *
 
 piece_set_path = "pieces/Maestro/"
-light_color = "beige"
-dark_color = "tan"
-fog_color = "grey"
+# index 0 is out of fog, index 1 is in fog
+light_colors = ["beige", "grey"]
+dark_colors = ["tan", "black"]
+# color to indicate that a square has been highlighted
 highlight_color = "orange"
-fog = [i for i in range(32, 64)]
 
-def draw_grid(win):
+# graphics are a bit slow, but it should be passable. each update undraws and redraws all items.
+# could possibly be made faster by 
+
+def draw_grid(win, fog):
     rects = []
     
     for i in range(64):
         p1 = Point((i%8+1)*100, int(i/8+1)*100)
         p2 = Point(i%8*100, int(i/8)*100)
         rects.append(Rectangle(p1, p2))
-        
+        in_fog = 0
         if i in fog:
-            rects[-1].setFill(fog_color)
-            rects[-1].setOutline(fog_color)
-            rects[-1].draw(win)
-        elif (int(i/8)+i%8)%2:
-            rects[-1].setFill(light_color)
-            rects[-1].setOutline(light_color)
+            in_fog = 1
+
+        if (int(i/8)+i%8)%2:
+            rects[-1].setFill(light_colors[in_fog])
+            rects[-1].setOutline(light_colors[in_fog])
             rects[-1].draw(win)
         else:
-            rects[-1].setFill(dark_color)
-            rects[-1].setOutline(dark_color)
+            rects[-1].setFill(dark_colors[in_fog])
+            rects[-1].setOutline(dark_colors[in_fog])
             rects[-1].draw(win)
-def draw_pieces(win, board):
+            
+def draw_pieces(win, board, fog):
     images = []
     for i in board:
         if not i[1] in fog:
@@ -41,25 +44,46 @@ def draw_board(board):
     Arguments:
         board: get from the return value of the board state
     """
-    
+    #print(board)
     win = GraphWin('Draw a Triangle', 800, 800, autoflush = False)
     win.setCoords(0, 0, 799, 799)
     win.setBackground('beige')
     
     # render the squares and pieces
-    draw_grid(win)
-    draw_pieces(win, board)
-    while 1:
-        pos = win.getMouse()
-        draw_grid(win)  # redraw board to overwrite previous highlights
-
-        rect = Rectangle(Point(int(pos.x/100 + 1)*100, int(pos.y/100 + 1)*100), 
-                         Point(int(pos.x/100)*100, int(pos.y/100)*100))
-        rect.setFill(highlight_color)
-        rect.setOutline(highlight_color)
-
-        rect.draw(win)  # draw highlight
-        draw_pieces(win, board) # draw pieces on the board
-
+    draw_grid(win, [i for i in range(16,64)])
+    draw_pieces(win, board, [i for i in range(16,64)])
+    return win
     
+def update_board(win, board, fog):
+    # convert x and y positions to a single square value
+    
+    for item in win.items[:]:
+        item.undraw()
+    draw_grid(win, fog)  # redraw board to overwrite previous highlights
+    draw_pieces(win, board, fog) # draw pieces on the board
+    
+    
+def get_update(win, board, fog = []):
+    """
+    Arguments:
+        board ([str, int] array): the true state of the board, in the form of an array of file names and index
+        fog (int array): the squares that are not visible to the current player.
+    Returns:
+        square (int): selected square
+    """
     pos = win.getMouse()
+    # convert x and y positions to a single square value
+    square = int(pos.x/100) + int(pos.y/100)*8
+    
+    for item in win.items[:]:
+        item.undraw()
+    update_board(win, board, fog)
+
+    rect = Rectangle(Point(int(pos.x/100 + 1)*100, int(pos.y/100 + 1)*100), 
+                     Point(int(pos.x/100)*100, int(pos.y/100)*100))
+    rect.setFill(highlight_color)
+    rect.setOutline(highlight_color)
+
+    rect.draw(win)  # draw highlight
+    draw_pieces(win, board, fog) # draw pieces on the board
+    return square
